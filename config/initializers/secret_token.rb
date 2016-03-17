a45ff7f85661dff26cb4dab479080c3a6389d3d7
Abuzhan <abuzhan.aidaraliyev@nu.edu.kz>
2016-03-17 12:42:32 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Abu::Application.config.secret_key_base = '8c84330aff00a7a529a7b5d45827f783f34b57eb97be60dc4fe2add3544c4179f57eb6055b008482dc180b5f6eced7929dec1d96d2c1d76d273f2aff47f76320'
